5742c60510bed2fcdf84a1ae56f2a3fd194ffe29
rawatravinder115 <rawatravinder115@gmail.com>
2021-04-06 23:27:57 +0530

    DAOs controller

---
+++ demo.JDBC-4/src/main/java/com/example/JDBC4/demo/JDBC4/DAOs/Person.java
@@ -0,0 +1,61 @@
+package com.example.JDBC4.demo.JDBC4.DAOs;
+
+// dao -> data access object.
+public class Person {   // this class represent table in a database
+
+    // mapping java object to table in DB.
+    private int id;
+    private String name;
+    private int age;
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public Person(int id, String name, int age, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    @Override
+    public String toString(){
+        return "name = +"+this.name+" , age = "+ this.age +", address = "+this.address;
+    }
+}
