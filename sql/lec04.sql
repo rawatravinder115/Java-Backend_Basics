7940d14b5f171a8496cb8009086df495a59b36f1
rawatravinder115 <59891757+rawatravinder115@users.noreply.github.com>
2021-04-14 23:58:06 +0530

    Delete sql directory

--- sql/lec04.sql
+++
@@ -1,8 +0,0 @@
-show databases;
-use persons;
-show tables;
-select * from persons;
-create database person_4;
-use person_4;
-show tables;
-select * from person;
--- sql/lec06.sql
+++
@@ -1,7 +0,0 @@
-show databases;
-drop database books;
-create database books;
-show tables;
-use books;
-select * from book;
-select * from my_book;
