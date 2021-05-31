5f5a3bc400494234cd95c7162aee543cb2b9c5a9
rawatravinder115 <rawatravinder115@gmail.com>
2021-05-31 13:10:58 +0530

    add important

--- Spring Security (JWT)/src/main/java/demo/springsecurityjwt/UserServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package demo.springsecurityjwt;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UserServiceRepository extends JpaRepository<MyUserDetails, Integer> {
-
-    public UserDetails findByUserName(String username);
-}
